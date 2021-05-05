b01e5b1a798897f3b2f92a2ba2d861c379d65841
juulioclf <juliolima731@gmail.com>
2021-05-04 20:38:54 -0400

    Update

--- Jogos/BatalhaNaval/batalhanaval.cpp
+++ Jogos/BatalhaNaval/batalhanaval.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include <string>
+
 
 using namespace std;
 
+void limpaTela(){
+    system("CLS");
+}
+
 int main(){
 
-    cout << "Batalha naval";
+    int opcao = 0;
+
+    while(opcao != 3){\
+
+        cout << "1: Jogar\n";
+        cout << "2: Sobre\n";
+        cout << "3: Sair\n";
+        cout << "Digite um valor e aperte ENTER: ";
+        cin >> opcao;
+
+        switch (opcao){
+            case 1:
+
+                cout << "Jogo iniciado\n";
+                break;
+
+            case 2:
+
+                cout << "Sobre\n";
+                break;
+
+            case 3:
+
+                cout << "Saindo do jogo ...\n";
+                break;
+        }
+    }
 
     return 0;
 }
